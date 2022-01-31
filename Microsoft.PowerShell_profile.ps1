1314931871cdb11d5ae2cc63e217ceed07a13006
Laurent Bergeron <lbergeron@photonic.com>
2022-01-31 16:09:04 -0700

    first commit, current settings

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,24 @@
+# Place this file in {User}/Documents/WindowsPowerShell/
+
+# Fix bug where ctrl+backspace prints "^W" to the console in vscode
+if ($env:TERM_PROGRAM -eq "vscode") {
+  Set-PSReadLineKeyHandler -Chord 'Ctrl+w' -Function BackwardKillWord
+}
+
+# Aliases
+Set-Alias -Name ipy -Value ipython
+Set-Alias -Name venv -Value .\venv\Scripts\activate
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.fu 'commit -m'
+git config --global alias.st status
+git config --global alias.last 'log -1 HEAD'
+
+function st() {
+    git status
+}
+
+function gp() {
+    git push
+}
