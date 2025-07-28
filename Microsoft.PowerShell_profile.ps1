0043ad398e3cc4420384a6649e5ef760d2daa5ab
Laurent Bergeron <lbergeron@photonic.com>
2025-07-28 16:45:42 -0600

    bring back undo and wip

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -27,6 +27,8 @@ Set-Alias -Name f -Value flake8
 
 git config --global core.longpaths true
 git config --global alias.last 'log -1 HEAD'
+git config --global alias.undo 'reset --soft HEAD^'
+git config --global alias.wip 'commit -m "wip"'
 git config --global push.default current
 
 function v() {
